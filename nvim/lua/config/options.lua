dbcac271501751ec98c95b1394662a11d5c4216a
Tobias Laross <Tobias.laross@assaabloy.com>
2024-02-21 19:20:23 +0100

    Improved telescope

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -39,3 +39,6 @@ vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
 
 -- Remove search highlight using esc
 vim.keymap.set("n", "<esc>", ":noh<CR><esc>", {silent = true, noremap = true})
+
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files theme=dropdown previewer=false<cr>", { desc = "Fuzzy find files in cwd" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep in cwd" })
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,23 +1,69 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    local telescope = require("telescope")
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {
+		defaults = {
+			prompt_prefix = "> ",
+			selection_caret = "> ",
+			entry_prefix = "  ",
+			multi_icon = "<>",
+			layout_strategy = "horizontal",
+			layout_config = {
+				width = 0.95,
+				height = 0.85,
+				prompt_position = "top",
+				horizontal = {
+					preview_width = function(_, cols, _)
+						if cols > 200 then
+							return math.floor(cols * 0.4)
+						else
+							return math.floor(cols * 0.6)
+						end
+					end,
+				},
+				vertical = {
+					width = 0.9,
+					height = 0.95,
+					preview_height = 0.5,
+				},
+				flex = {
+					horizontal = {
+						preview_width = 0.9,
+					},
+				},
+			},
+			selection_strategy = "reset",
+			sorting_strategy = "descending",
+			scroll_strategy = "cycle",
+			color_devicons = true,
+		},
 
-    telescope.setup({})
-
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    keymap.set(
-      "n",
-      "<leader>ff",
-      "<cmd>Telescope find_files theme=dropdown previewer=false<cr>",
-      { desc = "Fuzzy find files in cwd" }
-    )
-    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep in cwd" })
-  end,
+		pickers = {
+			find_files = {
+				find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+			},
+			lsp_implementations = {
+				find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+				layout_strategy = "vertical",
+				layout_config = {
+					prompt_position = "top",
+				},
+				sorting_strategy = "ascending",
+				ignore_filename = false,
+			},
+			live_grep = {
+				path_display = { "shorten" },
+				find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+				layout_strategy = "vertical",
+			},
+			grep_string = {
+				path_display = { "shorten" },
+				find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+				layout_strategy = "vertical",
+			},
+		},
+    },
 }
