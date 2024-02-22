4e5534153843b98202d3a9dd5ab6a9cf0ffb7c82
Tobias Laross <tobias.laross@gmail.com>
2024-02-22 12:37:21 +0100

    Added lspsaga

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,23 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "swift", "typescript", "javascript" },
+            ensure_installed = {
+                "c",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "swift",
+                "typescript",
+                "javascript",
+                "markdown",
+                "markdown_inline",
+            },
             auto_install = true,
             highlight = {
+                enable = true,
+            },
+            autopairs = {
                 enable = true,
             },
 
@@ -15,7 +29,7 @@ return {
                     node_incremental = "<Leader>si",
                     node_decremental = "<Leader>sd",
                 },
-            }
+            },
         })
     end,
 }
