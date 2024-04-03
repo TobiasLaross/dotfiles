905c23941e1ea921d71015952e000233e42d8348
Tobias Laross <Tobias.laross@assaabloy.com>
2024-04-03 16:13:03 +0200

    Minor improvements

--- nvim/lua/plugins/xcodebuild.lua
+++ nvim/lua/plugins/xcodebuild.lua
@@ -1,75 +1,79 @@
 local progress_handle
 
 return {
-    "wojciech-kulik/xcodebuild.nvim",
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        require("xcodebuild").setup({
-            show_build_progress_bar = false, -- shows [ ...    ] progress bar during build, based on the last duration
-            logs = {
-                notify = function(message, severity)
-                    local fidget = require("fidget")
-                    if progress_handle then
-                        progress_handle.message = message
-                        if not message:find("Loading") then
-                            progress_handle:finish()
-                            progress_handle = nil
-                            if vim.trim(message) ~= "" then
-                                fidget.notify(message, severity)
-                            end
-                        end
-                    else
-                        fidget.notify(message, severity)
-                    end
-                end,
-                notify_progress = function(message)
-                    local progress = require("fidget.progress")
+	"wojciech-kulik/xcodebuild.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("xcodebuild").setup({
+			show_build_progress_bar = false, -- shows [ ...    ] progress bar during build, based on the last duration
+			logs = {
+				notify = function(message, severity)
+					local fidget = require("fidget")
+					if progress_handle then
+						progress_handle.message = message
+						if not message:find("Loading") then
+							progress_handle:finish()
+							progress_handle = nil
+							if vim.trim(message) ~= "" then
+								fidget.notify(message, severity)
+							end
+						end
+					else
+						fidget.notify(message, severity)
+					end
+				end,
+				notify_progress = function(message)
+					local progress = require("fidget.progress")
 
-                    if progress_handle then
-                        progress_handle.title = ""
-                        progress_handle.message = message
-                    else
-                        progress_handle = progress.handle.create({
-                            message = message,
-                            lsp_client = { name = "xcodebuild.nvim" },
-                        })
-                    end
-                end,
-            },
-            code_coverage = {
-                enabled = true,
-            },
-            integrations = {
-                xcode_build_server = {
-                    enabled = true, -- run "xcode-build-server config" when scheme changes
-                },
-            },
-        })
+					if progress_handle then
+						progress_handle.title = ""
+						progress_handle.message = message
+					else
+						progress_handle = progress.handle.create({
+							message = message,
+							lsp_client = { name = "xcodebuild.nvim" },
+						})
+					end
+				end,
+			},
+			code_coverage = {
+				enabled = true,
+			},
+			integrations = {
+				xcode_build_server = {
+					enabled = true, -- run "xcode-build-server config" when scheme changes
+				},
+			},
+		})
 
-        vim.keymap.set("n", "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", { desc = "Toggle Xcodebuild Logs" })
-        vim.keymap.set("n", "<leader>xb", "<cmd>XcodebuildBuild<cr>", { desc = "Build Project" })
-        vim.keymap.set("n", "<leader>xx", "<cmd>XcodebuildBuildRun<cr>", { desc = "Build & Run Project" })
-        vim.keymap.set("n", "<leader>xr", "<cmd>XcodebuildRun<cr>", { desc = "Build & Run Project" })
-        vim.keymap.set("n", "<leader>xt", "<cmd>XcodebuildTest<cr>", { desc = "Run Tests" })
-        vim.keymap.set("n", "<leader>xT", "<cmd>XcodebuildTestClass<cr>", { desc = "Run This Test Class" })
-        vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", { desc = "Show All Xcodebuild Actions" })
-        vim.keymap.set(
-            "n",
-            "<leader>xf",
-            "<cmd>XcodebuildProjectManager<cr>",
-            { desc = "Show Project Manager Actions" }
-        )
-        vim.keymap.set("n", "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
-        vim.keymap.set("n", "<leader>xp", "<cmd>XcodebuildSelectTestPlan<cr>", { desc = "Select Test Plan" })
-        vim.keymap.set("n", "<leader>xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", { desc = "Toggle Code Coverage" })
-        vim.keymap.set(
-            "n",
-            "<leader>xC",
-            "<cmd>XcodebuildShowCodeCoverageReport<cr>",
-            { desc = "Show Code Coverage Report" }
-        )
-    end,
+		vim.keymap.set("n", "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", { desc = "Toggle Xcodebuild Logs" })
+		vim.keymap.set("n", "<leader>xö", function()
+			vim.cmd([[!tail -n 700 .nvim/xcodebuild/xcodebuild.log > .nvim/xcodebuild/tmplog]])
+			vim.cmd([[!mv .nvim/xcodebuild/tmplog .nvim/xcodebuild/xcodebuild.log]])
+		end, { desc = "Trim Xcodebuild Logs" })
+		vim.keymap.set("n", "<leader>xb", "<cmd>XcodebuildBuild<cr>", { desc = "Build Project" })
+		vim.keymap.set("n", "<leader>xx", "<cmd>XcodebuildBuildRun<cr>", { desc = "Build & Run Project" })
+		vim.keymap.set("n", "<leader>xr", "<cmd>XcodebuildRun<cr>", { desc = "Build & Run Project" })
+		vim.keymap.set("n", "<leader>xt", "<cmd>XcodebuildTest<cr>", { desc = "Run Tests" })
+		vim.keymap.set("n", "<leader>xT", "<cmd>XcodebuildTestClass<cr>", { desc = "Run This Test Class" })
+		vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", { desc = "Show All Xcodebuild Actions" })
+		vim.keymap.set(
+			"n",
+			"<leader>xf",
+			"<cmd>XcodebuildProjectManager<cr>",
+			{ desc = "Show Project Manager Actions" }
+		)
+		vim.keymap.set("n", "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
+		vim.keymap.set("n", "<leader>xp", "<cmd>XcodebuildSelectTestPlan<cr>", { desc = "Select Test Plan" })
+		vim.keymap.set("n", "<leader>xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", { desc = "Toggle Code Coverage" })
+		vim.keymap.set(
+			"n",
+			"<leader>xC",
+			"<cmd>XcodebuildShowCodeCoverageReport<cr>",
+			{ desc = "Show Code Coverage Report" }
+		)
+	end,
 }
