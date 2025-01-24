ce5d6507536b431d3d3cae80184ad3f86098eca2
Tobias Laross <tobias.laross@gmail.com>
2025-01-24 09:06:53 +0100

    Fixed broken oil on 14 inch screen

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,20 +3,24 @@ return {
 		"stevearc/oil.nvim",
 		---@module 'oil'
 		---@type oil.SetupOpts
-		opts = {
-			view_options = {
-				show_hidden = true,
-			},
-			float = {
-				padding = 2,
-				max_width = 0.4,
-				max_height = 0.6,
-				border = "rounded",
-				win_options = {
-					winblend = 0,
+		opts = function()
+			local max_width = math.floor(vim.o.columns * 0.4)
+			local max_height = math.floor(vim.o.lines * 0.6)
+			return {
+				view_options = {
+					show_hidden = true,
 				},
-			},
-		},
+				float = {
+					padding = 2,
+					max_width = max_width,
+					max_height = max_height,
+					border = "rounded",
+					win_options = {
+						winblend = 0,
+					},
+				},
+			}
+		end,
 		-- Optional dependencies
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
