90c662705bf00e5a7544a12d39e908ac35e14b17
Tobias Laross <Tobias.laross@assaabloy.com>
2025-04-29 08:46:59 +0200

    Added random characters into wordLogger

--- nvim/lua/util/word-logger.lua
+++ nvim/lua/util/word-logger.lua
@@ -1,26 +1,32 @@
 function LogWord()
     local word = vim.fn.expand("<cword>")
     local filetype = vim.bo.filetype
-    local log_statement = ""
+
+    local chars = {}
+    for i = 1, 4 do
+        chars[i] = string.char(math.random(65, 90))
+    end
+    local prefix = table.concat(chars) .. " TLA91:"
+    local stmt
 
     if filetype == "swift" then
-        log_statement = string.format("print(\"TLA91: %s = \\(%s)\")", word, word)
+        stmt = string.format("print(\"%s %s = \\(%s)\")", prefix, word, word)
     elseif filetype == "objc" or filetype == "objcpp" then
-        log_statement = string.format("NSLog(@\"TLA91: %s = %%@\", %s);", word, word)
+        stmt = string.format("NSLog(@\"%s %s = %%@\", %s);", prefix, word, word)
     elseif filetype == "typescript" or filetype == "javascript" then
-        log_statement = string.format("console.log('TLA91: %s = ', %s);", word, word)
+        stmt = string.format("console.log('%s %s = ', %s);", prefix, word, word)
     elseif filetype == "go" then
-        log_statement = string.format("fmt.Printf(\"TLA91: %s = %%v\\n\", %s)", word, word)
+        stmt = string.format("fmt.Printf(\"%s %s = %%v\\n\", %s)", prefix, word, word)
     elseif filetype == "sh" or filetype == "zsh" or filetype == "bash" then
-        log_statement = string.format("echo \"TLA91: %s = ${%s}\"", word, word)
+        stmt = string.format("echo \"%s %s = ${%s}\"", prefix, word, word)
     elseif filetype == "python" then
-        log_statement = string.format("print(f\"TLA91: %s = {%s}\")", word, word)
+        stmt = string.format("print(f\"%s %s = {%s}\")", prefix, word, word)
     elseif filetype == "java" then
-        log_statement = string.format("System.out.println(\"TLA91: %s = \" + %s);", word, word)
+        stmt = string.format("System.out.println(\"%s %s = \" + %s);", prefix, word, word)
     else
         print("No log statement defined for filetype: " .. filetype)
         return
     end
 
-    vim.api.nvim_put({ log_statement }, "l", true, false)
+    vim.api.nvim_put({ stmt }, "l", true, false)
 end
