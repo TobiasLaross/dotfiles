174d06a7ec37c3017b4814493b742da3ff188d60
Tobias Laross <tobias.laross@gmail.com>
2025-05-27 07:48:30 +0200

    Ghostty config

--- symlinks.sh
+++ symlinks.sh
@@ -8,5 +8,6 @@ mkdir -p ~/.config/tmux
 ln -sf $(pwd)/tmux/tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/tmux/conf ~/.config/tmux/
 ln -sf $(pwd)/OneDark.xccolortheme $(pwd)/../Library/Developer/Xcode/UserData/FontAndColorThemes
+ln -sf $(pwd)/ghostty/config /Users/tobias/Library/Application\ Support/com.mitchellh.ghostty/config
 
 . ~/.zshrc
