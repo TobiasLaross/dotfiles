52d9ea887ec44bfb87aa07224073c8f407eecc9f
Tobias Laross <Tobias.laross@assaabloy.com>
2025-05-30 07:40:23 +0200

    Updated aerospace workspaces

--- aerospace/chrome-workspace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-set -eu -o pipefail
-
-SCRIPT_DIR="$(dirname "$0")"
-chrome_window_id="$("$SCRIPT_DIR/get-window-id.sh" "Google Chrome")"
-aerospace move-node-to-workspace --window-id "$chrome_window_id" 2
