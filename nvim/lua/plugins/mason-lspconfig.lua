3931f25c59df4f00efc3bd46e6938fcce72fbd5e
Tobias Laross <tobias.laross@gmail.com>
2025-06-04 19:54:26 +0200

    Improved mason

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,20 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
-	opts = {},
+	opts = {
+		ensure_installed = {
+			"bashls",
+			"clangd",
+			"cssls",
+			"dockerls",
+			"html",
+			"pyright",
+			"rust_analyzer",
+			"vtsls",
+			"lua_ls",
+		},
+		automatic_enable = true,
+		automatic_installation = true,
+	},
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -4,19 +4,6 @@ return {
 	cmd = "Mason",
 	event = "BufReadPre",
 	opts = {
-		ensure_installed = {
-			"bashls",
-			"clangd",
-			"cssls",
-			"dockerls",
-			"html",
-			"pyright",
-			"rust_analyzer",
-			"vtsls",
-			"lua_ls",
-		},
-		automatic_enable = true,
-		automatic_installation = true,
 		ui = {
 			icons = {
 				package_installed = icons.symbols.check,
