367b9c0612664aa9e66ec72f64960f3f195f1b7e
Tobias Laross <Tobias.laross@assaabloy.com>
2025-11-03 10:58:07 +0100

    Updated marksman config

--- nvim/lua/plugins/marksman.lua
+++ nvim/lua/plugins/marksman.lua
@@ -1,28 +1,21 @@
 return {
-	"alexekdahl/marksman.nvim",
-	keys = {
-		{
-			"<leader>mk",
-			function()
-				require("marksman").goto_mark(5)
-			end,
-			desc = "Go to mark 5",
-		},
-	},
-	opts = {
-		keymaps = {
-			add = "<leader>ma",
-			show = "<leader>ms",
-			goto_1 = "<leader>mm",
-			goto_2 = "<leader>m,",
-			goto_3 = "<leader>m.",
-			goto_4 = "<leader>mj",
-		},
-		auto_save = true,
-		max_marks = 100,
-		search_in_ui = true,
-		sort_marks = false,
-		silent = true,
-		minimal = false,
-	},
+    "alexekdahl/marksman.nvim",
+    keys = {
+        { "<leader>ma", function() require("marksman").add_mark() end,   desc = "Add mark" },
+        { "<leader>ms", function() require("marksman").show_marks() end, desc = "Show marks" },
+        { "<leader>mm", function() require("marksman").goto_mark(1) end, desc = "Go to mark 1" },
+        { "<leader>m,", function() require("marksman").goto_mark(2) end, desc = "Go to mark 2" },
+        { "<leader>m.", function() require("marksman").goto_mark(3) end, desc = "Go to mark 3" },
+        { "<leader>mj", function() require("marksman").goto_mark(4) end, desc = "Go to mark 4" },
+        { "<leader>mk", function() require("marksman").goto_mark(5) end, desc = "Go to mark 5" },
+    },
+    opts = {
+        auto_save = true,
+        max_marks = 100,
+        search_in_ui = true,
+        sort_marks = false,
+        disable_default_keys = true,
+        silent = true,
+        minimal = true,
+    },
 }
