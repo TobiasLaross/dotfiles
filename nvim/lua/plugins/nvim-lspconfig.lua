e75cd8f6b57053bb1f0e9a47334b9ec1fe9c52a4
Tobias Laross <tobias.laross@gmail.com>
2025-11-04 08:04:09 +0100

    Added neovim dev env

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,37 +1,62 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "mason-org/mason.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-        local cmpNvimLsp = require("cmp_nvim_lsp")
-        local capabilities = cmpNvimLsp.default_capabilities()
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"folke/neodev.nvim",
+		"mason-org/mason.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	config = function()
+		require("neodev").setup()
+		local cmpNvimLsp = require("cmp_nvim_lsp")
+		local capabilities = cmpNvimLsp.default_capabilities()
 
-        local on_attach = function(_, _) end
+		local on_attach = function(_, _) end
 
-        vim.lsp.config.sourcekit = {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            cmd = {
-                "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
-            },
-        }
+		vim.lsp.config.lua_ls = {
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					diagnostics = { globals = { "vim" } },
+					workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+				},
+			},
+		}
 
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = { "swift", "objc", "c", "cpp", "objective-cpp" },
-            callback = function(event)
-                vim.lsp.start({
-                    name = "sourcekit",
-                    config = vim.lsp.config.sourcekit,
-                    root_dir = vim.fs.root(event.buf,
-                        { "buildServer.json", "*.xcodeproj", "*.xcworkspace", ".git", "Package.swift" }),
-                })
-            end,
-        })
-    end,
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "lua",
+			callback = function(event)
+				vim.lsp.start({
+					name = "lua_ls",
+					config = vim.lsp.config.lua_ls,
+					root_dir = vim.fs.root(event.buf, { ".git" }),
+				})
+			end,
+		})
+		vim.lsp.config.sourcekit = {
+			on_attach = on_attach,
+			capabilities = capabilities,
+			cmd = {
+				"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
+			},
+		}
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "swift", "objc", "c", "cpp", "objective-cpp" },
+			callback = function(event)
+				vim.lsp.start({
+					name = "sourcekit",
+					config = vim.lsp.config.sourcekit,
+					root_dir = vim.fs.root(
+						event.buf,
+						{ "buildServer.json", "*.xcodeproj", "*.xcworkspace", ".git", "Package.swift" }
+					),
+				})
+			end,
+		})
+	end,
 }
