7e85876b756bd19f848fb633140d4cdf7b6d30c0
Tobias Laross <tobias.laross@gmail.com>
2025-11-17 10:48:37 +0100

    Updated wordlogger

--- nvim/lua/util/word-logger.lua
+++ nvim/lua/util/word-logger.lua
@@ -1,32 +1,34 @@
 function LogWord()
-    local word = vim.fn.expand("<cword>")
-    local filetype = vim.bo.filetype
+	local word = vim.fn.expand("<cword>")
+	local filetype = vim.bo.filetype
 
-    local chars = {}
-    for i = 1, 4 do
-        chars[i] = string.char(math.random(65, 90))
-    end
-    local prefix = table.concat(chars) .. " TLA91:"
-    local stmt
+	local chars = {}
+	for index = 1, 4 do
+		chars[index] = string.char(math.random(65, 90))
+	end
+	local prefix = table.concat(chars) .. " TLA91:"
+	local stmt
 
-    if filetype == "swift" then
-        stmt = string.format("print(\"%s %s = \\(%s)\")", prefix, word, word)
-    elseif filetype == "objc" or filetype == "objcpp" then
-        stmt = string.format("NSLog(@\"%s %s = %%@\", %s);", prefix, word, word)
-    elseif filetype == "typescript" or filetype == "javascript" then
-        stmt = string.format("console.log('%s %s = ', %s);", prefix, word, word)
-    elseif filetype == "go" then
-        stmt = string.format("fmt.Printf(\"%s %s = %%v\\n\", %s)", prefix, word, word)
-    elseif filetype == "sh" or filetype == "zsh" or filetype == "bash" then
-        stmt = string.format("echo \"%s %s = ${%s}\"", prefix, word, word)
-    elseif filetype == "python" then
-        stmt = string.format("print(f\"%s %s = {%s}\")", prefix, word, word)
-    elseif filetype == "java" then
-        stmt = string.format("System.out.println(\"%s %s = \" + %s);", prefix, word, word)
-    else
-        print("No log statement defined for filetype: " .. filetype)
-        return
-    end
+	if filetype == "swift" then
+		stmt = string.format('print("%s %s = \\(%s)")', prefix, word, word)
+	elseif filetype == "objc" or filetype == "objcpp" then
+		stmt = string.format('NSLog(@"%s %s = %%@", %s);', prefix, word, word)
+	elseif filetype == "typescript" or filetype == "javascript" then
+		stmt = string.format("console.log('%s %s = ', %s);", prefix, word, word)
+	elseif filetype == "go" then
+		stmt = string.format('fmt.Printf("%s %s = %%v\\n", %s)', prefix, word, word)
+	elseif filetype == "sh" or filetype == "zsh" or filetype == "bash" then
+		stmt = string.format('echo "%s %s = ${%s}"', prefix, word, word)
+	elseif filetype == "python" then
+		stmt = string.format('print(f"%s %s = {%s}")', prefix, word, word)
+	elseif filetype == "java" then
+		stmt = string.format('System.out.println("%s %s = " + %s);', prefix, word, word)
+	elseif filetype == "rust" then
+		stmt = string.format('println!("%s %s = {:?}", %s);', prefix, word, word)
+	else
+		print("No log statement defined for filetype: " .. filetype)
+		return
+	end
 
-    vim.api.nvim_put({ stmt }, "l", true, false)
+	vim.api.nvim_put({ stmt }, "l", true, false)
 end
