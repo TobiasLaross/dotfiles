7e9ae1c48e2ce6dddad835216914e9b5dbc855b6
Tobias Laross <tobias.laross@gmail.com>
2026-03-05 06:50:17 +0100

    Updated snacks format

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,21 +1,9 @@
 return {
-    "folke/snacks.nvim",
-    event = "VeryLazy",
-    opts = {
-        explorer = {
-            -- your explorer configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        gitbrowse = {
-            -- your gitbrowse configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        notifier = {
-            -- your notifier configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    }
+	"folke/snacks.nvim",
+	event = "VeryLazy",
+	opts = {
+		explorer = {},
+		gitbrowse = {},
+		notifier = {},
+	},
 }
